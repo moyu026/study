c03e3277f7e232939f995ed795ea28ed96fbd749
moyu026 <74128024+moyu026@users.noreply.github.com>
2022-06-25 09:23:37 +0800

    Delete 2.torchvision.datasets.ImageFolder.py

--- 2.torchvision.datasets.ImageFolder.py
+++
@@ -1,8 +0,0 @@
-from torchvision import datasets
-
-datapath = r'E:\github\dataset\\test'
-train_dataset = datasets.ImageFolder(datapath)
-flower_list = train_dataset.class_to_idx
-
-train_num = len(train_dataset)
-
